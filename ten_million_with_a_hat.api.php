9fedf013a2d38349a534465e1c9445693899093e
qadan <daitken@discoverygarden.ca>
2015-02-24 16:53:22 -0400

    refactor of things

--- ten_million_with_a_hat.api.php
+++ ten_million_with_a_hat.api.php
@@ -6,25 +6,34 @@
  */
 
 /**
- * Allows additional callbacks to execute after each object has been ingested.
+ * Allows additional callbacks to execute during the batch process.
  *
- * Note that this callback hook gets called before any object is ingested. If
- * you need any tasks to be run before the batch process begins, or need any
- * static variables to be generated and passed to the entire batch process,
- * consider doing it here.
+ * Also allows for the return of messages from these callbacks. As all keys in
+ * the hook array are optional, one can skip the callback array and simply pass
+ * back a 'message' for display purposes during the batch. It would not be much
+ * use to implement this hook without a 'callback' or 'message' in the array,
+ * though, so probably don't do that.
  *
  * @return array
  *   An associative array of callback arrays to execute, containing:
- *   - 'file' (optional): an array containing 'type', 'module' and 'path', as
- *   one would pass these parameters to module_load_include().
+ *   - 'file': an array containing 'type', 'module' and 'path', as
+ *     one would pass these parameters to module_load_include().
  *   - 'callback': the name or class/method array for call_user_func_array()
- *   to execute. Note that the callback must accept the Fedora object currently
- *   being worked with as the first argument, and must return that same object.
- *   - 'args' (optional): an array of arguments acceptable to
- *   call_user_func_array(). Added to the beginning of this will be the Fedora
- *   object being worked with.
- *   - 'weight' (optional): a designated weight for the execution order of
- *   callbacks.
+ *     to execute. Note that callbacks run using the between_ingests 'when'
+ *     must accept the Fedora object currently being worked with as the first
+ *     argument. If the callback returns a string, it will be used as a message
+ *     for drush to print out, provided that the 'message' is not set in this
+ *     hook array.
+ *   - 'args': an array of arguments acceptable to
+ *     call_user_func_array(). Added to the beginning of this will be the Fedora
+ *     object being worked with.
+ *   - 'weight': a designated weight for the execution order of
+ *     callbacks.
+ *   - 'message': A message to show after the callback is executed.
+ *     Overrides any return value from the callback.
+ *   - 'when': one of three possible values - 'before_batch',
+ *     'between_ingests', or 'after_batch', to designate when this should be
+ *     run. Defaults to 'between_ingests'.
  */
 function hook_ten_million_with_a_hat_also_do_these_things() {
   $colour = hat_colour_get_colour('blue');
@@ -37,6 +46,8 @@ function hook_ten_million_with_a_hat_also_do_these_things() {
       ),
       'callback' => 'add_second_hat_put_on_second_hat',
       'weight' => 5,
+      'when' => 'before_batch',
+      'message' => 'More hats!',
     ),
     'Change the hat colour to blue!' => array(
       // This theoretical callback is part of the .module and requires no file
@@ -46,48 +57,7 @@ function hook_ten_million_with_a_hat_also_do_these_things() {
       'args' => array($colour),
       // Plus, this theoretical callback should be run before the one above.
       'weight' => 4,
-    ),
-  );
-}
-
-/**
- * Messaging for hats! Say things on the hats.
- *
- * Each key returned by this callback must be set to an associative array
- * containing the following keys:
- * - 'message': a message to display. Overrides 'callback'.
- * - 'callback': a string containing the name of a callback to run to get the
- *   message. Callbacks should return a string representing a message to
- *   display, or return NULL to skip displaying a message.
- * - 'type': a string containing the type of message being generated, either
- *   'before_batch' to display it before the entire batch process,
- *   'between_ingests' to display it once after each object is ingested, and 
- *   'after_batch' to display it after all objects have been ingested. Note
- *   that like the above hook_ten_million_with_a_hat_also_do_these_things(),
- *   'between_ingests' callbacks require the current AbstractObject being
- *   worked with as their first parameter.
- * - 'args (optional)': an array of arguments to pass to the callback. In the case of
- *   'between_ingests', these arguments will be preceded by the AbstractObject
- *   currently being worked with.
- * - 'weight (optional)': a numeric weight value to designate which messages
- *   should display before which.
- */
-function hook_ten_million_with_a_hat_also_say_these_things() {
-  return array(
-    array(
-      'type' => 'before_batch',
-      'callback' => 'hat_colour_before_batch_mesage_callback',
-      'args' => array('blue'),
-    ),
-    array(
-      'type' => 'between_ingests',
-      'callback' => 'hat_colour_between_ingests_message_callback',
-      'weight' => 10,
-      'args' => array('Param 2', 'Param 3'),
-    ),
-    array(
-      'type' => 'after_batch',
-      'message' => "All done!",
+      // We could put a 'when' here, but it defaults to between_ingests.
     ),
   );
 }
@@ -99,41 +69,11 @@ function hook_ten_million_with_a_hat_also_say_these_things() {
  *   The object being worked with.
  * @param string $new_colour
  *   The new colour to use.
+ *
+ * @return string
+ *   The message to display.
  */
 function hat_colour_change_colour(AbstractObject $object, $new_colour) {
   $object->colour = $new_colour;
-}
-
-/**
- * Our theoretical hat colour getter.
- *
- * @param string $colour
- *   It's a colour.
- *
- * @return string
- *   A hat colour valid for use with colour-compatible objects.
- */
-function hat_colour_get_colour($colour) {
-  // Do some rainbow magic here.
-  return $colour;
-}
-
-/**
- * Just an example of what the messaging callbacks should look like.
- *
- * @param AbstractObject $object
- *   The object being worked with, since this is a between_ingests callback.
- * @param string $param_2
- *   A theoretical second parameter.
- * @param string $param_3
- *   A theoretical third parameter.
- *
- * @return string|null
- *   A string containing the message, or NULL if we should return no message.
- */
-function hat_colour_between_ingests_message_callback(AbstractObject $object, $param_2, $param_3) {
-  if ($object->id === "some:exception") {
-    return NULL;
-  }
-  return "The object was {$object->id}, and the parameters were $param_2 and $param_3";
+  return "Hat colour is now $new_colour.";
 }
