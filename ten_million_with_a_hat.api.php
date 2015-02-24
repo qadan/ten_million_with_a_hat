edf814ad8ca5ecd9a5930557199b7d2fd5f7e464
qadan <daitken@discoverygarden.ca>
2015-02-23 23:25:14 -0400

    messaging and fixes

--- ten_million_with_a_hat.api.php
+++ ten_million_with_a_hat.api.php
@@ -51,6 +51,45 @@ function hook_ten_million_with_a_hat_also_do_these_things() {
 }
 
 /**
+ * Messaging for hats! Say things on the hats.
+ *
+ * Each key returned by this callback must be set to an associative array
+ * containing the following keys:
+ * - 'callback': a string containing the name of the callback being referenced
+ * - 'type': a string containing the type of message being generated, either
+ *   'before_batch' to display it before the entire batch process,
+ *   'between_ingests' to display it once after each object is ingested, and 
+ *   'after_batch' to display it after all objects have been ingested. Note
+ *   that like the above hook_ten_million_with_a_hat_also_do_these_things(),
+ *   'between_ingests' callbacks require the current AbstractObject being
+ *   worked with as their first parameter.
+ * - 'args (optional)': an array of arguments to pass to the callback. In the case of
+ *   'between_ingests', these arguments will be preceded by the AbstractObject
+ *   currently being worked with.
+ * - 'weight (optional)': a numeric weight value to designate which messages
+ *   should display before which.
+ */
+function hook_ten_million_with_a_hat_also_say_these_things() {
+  return array(
+    array(
+      'type' => 'before_batch',
+      'callback' => 'hat_colour_before_batch_mesage_callback',
+      'args' => array('blue'),
+    ),
+    array(
+      'type' => 'between_ingests',
+      'callback' => 'hat_colour_between_ingests_message_callback',
+      'weight' => 10,
+      'args' => array('Param 2', 'Param 3'),
+    ),
+    array(
+      'type' => 'after_batch',
+      'callback' => 'hat_colour_after_batch_message_callback',
+    ),
+  );
+}
+
+/**
  * Just an example of what the callbacks should look like.
  *
  * @param AbstractObject $object
@@ -73,5 +112,5 @@ function hat_colour_change_colour(AbstractObject $object, $new_colour) {
  */
 function hat_colour_get_colour($colour) {
   // Do some rainbow magic here.
-  return $colour
+  return $colour;
 }
